96e17d4d2eecb700594dc598e92b87fd969cece0
Noam Sellam <noamsellam@noams-imac.local>
2016-09-14 15:29:43 +0300

    Initial Commit

---
+++ BasicCommands/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BasicCommands
+//
+//  Created by Noam Sellam on 14/09/2016.
+//  Copyright © 2016 Noam Sellam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
